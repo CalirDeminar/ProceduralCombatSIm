5f64d8dd2a1ad21b9dee434e0667fe4328ca04d3
Edward Glen <edward.glen@se.com>
2023-05-04 12:56:11 +0100

    add health recalculation tests

--- src/creature.rs
+++ src/creature.rs
@@ -86,9 +86,15 @@ pub mod creature {
         let working_breath_ratio = get_ratio_of_working_body_tags(&subject.body, BodyPartTag::breath);
         let working_circulation_ratio = get_ratio_of_working_body_tags(&subject.body, BodyPartTag::circulation);
 
+
+
         // multipler shouldn't be above one, and hearth failure is more severe than breath trouble / failure
-        let breath_loss_factor = working_breath_ratio.max(working_circulation_ratio * 2.0).min(1.0) * BREATH_LOSS_RATE;
-        subject.health_stats.bloodOxyPtc -=  breath_loss_factor.min(0.0);
+        let breath_loss_factor = (1.0 - working_breath_ratio)
+            .max(1.0 - (working_circulation_ratio * 2.0))
+            .min(1.0)
+            .max(0.0) * BREATH_LOSS_RATE;
+
+        subject.health_stats.bloodOxyPtc -=  breath_loss_factor.max(0.0);
 
         let mut total_blood_loss = 0.0;
         let parts = flatten_all(&subject.body);
@@ -112,15 +118,43 @@ pub mod creature {
 
 #[cfg(test)]
 mod tests {
-    use crate::creature::{humanoid::humanoid::humanoid, body::body::random_weighted_part_with_internal};
+    use crate::creature::creature::*;
+    use crate::creature::body::body::*;
+    use crate::creature::humanoid::humanoid::*;
 
     #[test]
-    fn flatten() {
-        let subject = humanoid();
-        println!("{:#?}", subject);
-        for _i in 0..100 {
-            let part = random_weighted_part_with_internal(&subject.body);
-            println!("{:?}", part.name);
-        }
+    fn recalculate_health_healthy() {
+        let mut subject = humanoid();
+        recalculate_health(&mut subject);
+        assert_eq!(subject.health_stats.bloodVolPtc, 1.0);
+        assert_eq!(subject.health_stats.bloodOxyPtc, 1.0);
+    }
+
+    #[test]
+    fn recalculate_health_oxy() {
+        let mut subject = humanoid();
+        subject.body.internal
+            .iter_mut()
+            .find(|p| p.tags.contains(&BodyPartTag::breath))
+            .unwrap()
+            .statuses.push(BodyPartStatus::paralised);
+
+        recalculate_health(&mut subject);
+        assert_eq!(subject.health_stats.bloodVolPtc, 1.0);
+        assert_eq!(subject.health_stats.bloodOxyPtc < 1.0, true);
+    }
+
+    #[test]
+    fn recalculate_health_blood_vol() {
+        let mut subject = humanoid();
+        subject.body.children
+            .iter_mut()
+            .find(|p| p.name.eq("Left Leg"))
+            .unwrap()
+            .statuses.push(BodyPartStatus::missing);
+
+        recalculate_health(&mut subject);
+        assert_eq!(subject.health_stats.bloodOxyPtc, 1.0);
+        assert_eq!(subject.health_stats.bloodVolPtc < 1.0, true);
     }
 }
